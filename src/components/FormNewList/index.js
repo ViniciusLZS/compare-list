16ef2caf63327512284001e7421eaf9a6ad2650e
Vinícius <vinicius.luiz.9256@gmail.com>
2023-04-29 16:38:19 -0300

    feat: character limit

--- src/components/FormNewList/index.js
+++ src/components/FormNewList/index.js
@@ -38,6 +38,7 @@ export default function Form() {
           placeholder="Nome do estabelecimento ou marca"
           onChange={(event) => handleStoreChange(event)}
           value={nameStore}
+          maxLength="25"
         />
       </FormGroup>
 
