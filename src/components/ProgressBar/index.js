54e35bd4fb2d9967346dd72ca95d001c66f32ffa
Vinícius <vinicius.luiz.9256@gmail.com>
2023-04-29 16:42:00 -0300

    feat: created progress bar

--- src/components/ProgressBar/index.js
+++ src/components/ProgressBar/index.js
@@ -2,6 +2,16 @@ import * as S from './styles';
 
 export default function ProgressBar() {
   return (
-    <S.ContainerBar />
+    <S.ContainerBar>
+      <S.Bar>
+        <div className="progress">
+          <span>100.00</span>
+        </div>
+      </S.Bar>
+
+      <S.ContainerSpan>
+        <span>100.00</span>
+      </S.ContainerSpan>
+    </S.ContainerBar>
   );
 }
--- src/components/ProgressBar/styles.js
+++ src/components/ProgressBar/styles.js
@@ -1,8 +1,50 @@
 import styled from 'styled-components';
 
 export const ContainerBar = styled.div`
-  background: ${({ theme }) => theme.colors.primary.dark};
-
   width: 100%;
-  height: 5rem;
+  max-height: 6rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+export const Bar = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  max-width: 45rem;
+  height: 2rem;
+  border: 1px solid ${({ theme }) => theme.colors.gray[200]};
+  border-radius: 4rem;
+
+  .progress {
+    background: ${({ theme }) => theme.colors.primary.main};
+    border-radius: 4rem;
+    width: 100%;
+    height: 1.5rem;
+    margin: 0 0.2rem;
+    padding-left: 4rem;
+
+    display: flex;
+    justify-content: end;
+
+    span {
+      font-size: 1.2rem;
+      padding-right: 0.2rem;
+    }
+  }
+`;
+
+export const ContainerSpan = styled.div`
+  display: flex;
+  width: 100%;
+  max-width: 45rem;
+  height: 2rem;
+  justify-content: flex-end;
+  align-items: end;
+
+  span{
+    font-size: 1.2rem;
+  }
 `;
