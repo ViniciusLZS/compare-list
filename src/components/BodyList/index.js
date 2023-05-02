47c4554387f8fd7ba695567abf35787e9cc18ee4
Vinícius <vinicius.luiz.9256@gmail.com>
2023-05-02 16:43:54 -0300

    feat: variable added in array in controller

--- src/components/BodyList/index.js
+++ src/components/BodyList/index.js
@@ -9,37 +9,10 @@ import Trash from '../../assets/image/icons/bin.svg';
 export default function BodyList({ view }) {
   useEffect(() => {
     window.scrollTo(0, document.body.scrollHeight - window.innerHeight);
-  }, []);
+  }, [view]);
 
   return (
     <S.Container view={view}>
-      <S.Card view={view}>
-        <S.Content view={view}>
-          <S.Title view={view}>Maçã</S.Title>
-
-          <S.Image view={view}>
-            <img src={Apple} alt="Maçã" />
-          </S.Image>
-
-          <S.ContainerValue view={view}>
-            <div className="values">
-              <span>2 Unid.</span>
-              <span>R$ 7,50</span>
-            </div>
-
-            <div className="total">
-              <span>R$ 15,00</span>
-            </div>
-          </S.ContainerValue>
-
-          <S.Trash>
-            <button type="button">
-              <img src={Trash} alt="Lixeira" />
-            </button>
-          </S.Trash>
-        </S.Content>
-      </S.Card>
-
       <S.Card view={view}>
         <S.Content view={view}>
           <S.Title view={view}>Maçã</S.Title>
