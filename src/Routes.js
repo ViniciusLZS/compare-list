f0fe3d26ad646a2888a5b7737e9d967ebae1cced
Vinícius <vinicius.luiz.9256@gmail.com>
2023-05-02 10:21:49 -0300

    feat: modified switch and route

--- src/Routes.js
+++ src/Routes.js
@@ -1,17 +1,15 @@
-import { Switch, Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
-import Home from './pages/Home';
-// import Header from './components/Header';
 import NewList from './pages/NewList';
 import List from './pages/List';
+import Logo from './components/Logo';
 
 export default function Routes() {
   return (
-    <Switch>
-      <Route path="/" exact component={Home} />
-      {/* <Route component={Header} /> */}
+    <>
+      <Logo />
       <Route path="/new" component={NewList} />
       <Route path="/list" component={List} />
-    </Switch>
+    </>
   );
 }
--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,10 +1,10 @@
 import { ThemeProvider } from 'styled-components';
 
-import { BrowserRouter } from 'react-router-dom';
+import { Route, BrowserRouter, Switch } from 'react-router-dom';
 import GlobalStyles from '../../assets/styles/global';
 import defaultTheme from '../../assets/styles/themes/default';
 import * as S from './styles';
-// import Header from '../Header';
+import Home from '../../pages/Home';
 import Routes from '../../Routes';
 
 function App() {
@@ -13,8 +13,10 @@ function App() {
       <ThemeProvider theme={defaultTheme}>
         <GlobalStyles />
         <S.Container>
-          {/* <Header /> */}
-          <Routes />
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Routes />
+          </Switch>
         </S.Container>
       </ThemeProvider>
     </BrowserRouter>
