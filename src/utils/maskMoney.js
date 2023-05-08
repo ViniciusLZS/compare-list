9584c20e38376474a37e1c61ffa3bf57d4c11592
Vinícius <vinicius.luiz.9256@gmail.com>
2023-05-08 12:24:56 -0300

    refactor: added characters in value

--- src/utils/maskMoney.js
+++ src/utils/maskMoney.js
@@ -1,5 +1,12 @@
-export default function maskMoney(money) {
-  return money
-    .replace(/\D/g, '')
-    .replace(/(\d{1})(\d{3})(\d{2})/, '$1.$2,$3');
+export default function maskMoney(value) {
+  // Remove tudo o que não é dígito numérico
+  let newValue = value.replace(/\D/g, '');
+
+  // Formata o valor em moeda com duas casas decimais
+  newValue = (newValue / 100).toFixed(2).replace('.', ',');
+
+  // Adiciona o símbolo de real
+  newValue = `R$ ${newValue.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.')}`;
+
+  return newValue;
 }
