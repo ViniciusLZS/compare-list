23f45e94793be860d1943472f59de9a8a7a26def
Vinícius <vinicius.luiz.9256@gmail.com>
2023-05-09 17:19:34 -0300

    feat: new request to list for stores

--- src/services/StoreService.js
+++ src/services/StoreService.js
@@ -1,4 +1,12 @@
 class StoreService {
+  async listsStore() {
+    const response = await fetch('https://64530c54bce0b0a0f7547089.mockapi.io/comparelist/v1/newlist', {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' },
+    });
+    return response.json();
+  }
+
   async listStore(params) {
     const response = await fetch(`https://64530c54bce0b0a0f7547089.mockapi.io/comparelist/v1/newlist/${params.id}`, {
       method: 'GET',
