7893022d6bef93a4a2ffe6bad7410d87726fc200
Vinícius <vinicius.luiz.9256@gmail.com>
2023-05-09 17:12:35 -0300

    feat: route added

--- src/Routes.js
+++ src/Routes.js
@@ -3,6 +3,7 @@ import { Route } from 'react-router-dom';
 import NewList from './pages/NewList';
 import List from './pages/List';
 import Logo from './components/Logo';
+import MyLists from './pages/MyLists';
 
 export default function Routes() {
   return (
@@ -10,6 +11,7 @@ export default function Routes() {
       <Logo />
       <Route path="/new" component={NewList} />
       <Route path="/list/:id" component={List} />
+      <Route path="/mylists" component={MyLists} />
     </>
   );
 }
