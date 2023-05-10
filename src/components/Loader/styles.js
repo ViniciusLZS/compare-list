1c3c779645fdd722eefd615300a4e683cfd40e5e
Vinícius <vinicius.luiz.9256@gmail.com>
2023-05-10 15:29:11 -0300

    refactor: componentized charger

--- src/components/Loader/styles.js
+++ src/components/Loader/styles.js
@@ -1,5 +1,12 @@
 import styled from 'styled-components';
 
 export const Overlay = styled.div`
-  
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
