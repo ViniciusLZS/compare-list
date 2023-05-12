395c31a7cde090d844822c7bbf0062f92ef0d3d1
Vinícius <vinicius.luiz.9256@gmail.com>
2023-05-12 15:27:52 -0300

    refactor: title break

--- src/components/PageHeader/styles.js
+++ src/components/PageHeader/styles.js
@@ -17,15 +17,13 @@ export const Span = styled.span`
   font-size: 2rem;
   max-height: 5rem;
   text-align: center;
-  word-break: break-word;
-  overflow-y: scroll;
-  padding: 0 0.5rem;
+  overflow-x: scroll;
+  white-space: nowrap;
+
 
   @media(min-width: 700px) {
     font-size: 2rem;
-    overflow-y: hidden;
-
-  }
+    }
 `;
 
 export const FormatView = styled.div`
@@ -48,6 +46,6 @@ export const FormatView = styled.div`
   @media(min-width: 700px) {
     img {
     width: 3rem;
-  }
+    }
   }
 `;
