082aa8d2e9248d319e81b3ccf2c78cc4859f7e12
Vinícius <vinicius.luiz.9256@gmail.com>
2023-05-24 17:08:05 -0300

    style: removed comments

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,29 +1,6 @@
-// import { useContext } from 'react';
-
-// import { useHistory } from 'react-router-dom';
-// import { AuthContext } from '../../context/AuthContext';
-
 import FormSignIn from '../../components/Forms/FomSignIn';
-// import UserService from '../../services/UserService';
 
 export default function SignIn() {
-  // const { setLogin } = useContext(AuthContext);
-  // const history = useHistory();
-
-  // const handleSubmit = async (formData) => {
-  //   try {
-  //     const response = await UserService.login(formData);
-  //     const { token } = await response.json();
-
-  //     window.localStorage.setItem('token', token);
-
-  //     history.push(`/profile/${response.token}`);
-  //   } catch (error) {
-  //     setLogin(false);
-  //     console.log({ error });
-  //   }
-  // };
-
   return (
     <FormSignIn />
   );
