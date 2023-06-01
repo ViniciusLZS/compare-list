394f80a8b22679c74391c0cd4b0dfdc7f9c4f4ed
Vinícius <vinicius.luiz.9256@gmail.com>
2023-06-01 19:15:18 -0300

    feat: handle modal

--- src/components/Modal/ContainerModal/index.tsx
+++ src/components/Modal/ContainerModal/index.tsx
@@ -1,33 +1,44 @@
 /* eslint-disable react/require-default-props */
-import { ReactNode } from 'react';
+import { ReactNode, MouseEvent } from 'react';
 import reactDom from 'react-dom';
 
 import * as S from './styles';
 
-import Button from '../../Button';
+// import Button from '../../Button';
 
 import CloseIcon from '../../../assets/image/icons/close.svg';
 
 interface ContainerModalProps {
   danger?: boolean;
   children: ReactNode;
+  handleModal: () => void;
 }
 
-export default function ContainerModal({ danger = false, children }: ContainerModalProps) {
+export default function ContainerModal(
+  { danger = false, children, handleModal }: ContainerModalProps,
+) {
   const modalRoot = document.getElementById('modal-root');
+
+  function handleClick(event: MouseEvent<HTMLDivElement | HTMLButtonElement>) {
+    if (event.target === event.currentTarget) {
+      handleModal();
+    }
+  }
 
   if (!modalRoot) {
     return null;
   }
   return reactDom.createPortal(
-    <S.Overlay>
+    <S.Overlay onClick={(event) => handleClick(event)}>
       <S.Container danger={danger}>
-        <S.Close>
-          <img src={CloseIcon} alt="x" />
-        </S.Close>
+        <button type="button" onClick={handleModal}>
+          <S.Close>
+            <img src={CloseIcon} alt="x" />
+          </S.Close>
+        </button>
         {children}
 
-        <S.Footer>
+        {/* <S.Footer>
           <button type="button" className="cancel-button">
             Cancelar
           </button>
@@ -35,7 +46,7 @@ export default function ContainerModal({ danger = false, children }: ContainerMo
           <Button type="button" danger={danger}>
             Deletar
           </Button>
-        </S.Footer>
+        </S.Footer> */}
       </S.Container>
     </S.Overlay>,
     modalRoot,
