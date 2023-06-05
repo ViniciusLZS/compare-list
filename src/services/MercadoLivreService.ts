c4921e426efeb9b1e8d368d7d5c49e2a9f62b243
Vinícius <vinicius.luiz.9256@gmail.com>
2023-06-05 17:13:00 -0300

    feat: categoryId

--- src/services/MercadoLivreService.ts
+++ src/services/MercadoLivreService.ts
@@ -7,7 +7,7 @@ class CategoriesService {
     this.httpClient = new HttpClient('http://localhost:3001');
   }
 
-  async listAll(token: string) {
+  async listAllCategories(token: string) {
     return this.httpClient.get('/api/categories', {
       headers: {
         Authorization: `Bearer ${token}`,
@@ -15,13 +15,14 @@ class CategoriesService {
     });
   }
 
-  // async getList({ id, token }: {id: string; token: string;}) {
-  //   return this.httpClient.get(`/list/${id}`, {
-  //     headers: {
-  //       Authorization: `Bearer ${token}`,
-  //     },
-  //   });
-  // }
+  async listAllProducts({ name, categoriesId, token }
+    :{name: string; categoriesId: string; token: string;}) {
+    return this.httpClient.get(`/api/products/${categoriesId}/${name}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+  }
 }
 
 export default new CategoriesService();
