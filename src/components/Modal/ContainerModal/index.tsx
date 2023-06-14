d14716223bb6d942984ba1ede346e22aaac3fff9
Vinícius <vinicius.luiz.9256@gmail.com>
2023-06-13 22:10:58 -0300

    feat: added ReactPortal, props and functions

--- src/components/Modal/ContainerModal/index.tsx
+++ src/components/Modal/ContainerModal/index.tsx
@@ -1,51 +1,110 @@
 /* eslint-disable react/require-default-props */
 import { ReactNode, MouseEvent } from 'react';
-import reactDom from 'react-dom';
 
 import * as S from './styles';
 
-// import Button from '../../Button';
-
 import CloseIcon from '../../../assets/image/icons/close.svg';
+import ReactPortal from '../../ReactPortal';
+import Button from '../../Button';
 
 interface ContainerModalProps {
   danger?: boolean;
   children: ReactNode;
-  handleModal: () => void;
-  handleDropdown: () => void;
+  handleModal?: (() => void) | undefined;
+  handleDropdown?: () => void;
+  title: string;
+  cancelLabel?: string;
+  confirmLabel?: string;
+  onCancel?: () => void;
+  onConfirm?: () => void;
+  visible?: boolean;
+  isLoading?: boolean;
 }
 
 export default function ContainerModal(
   {
-    danger = false, children, handleModal, handleDropdown,
+    danger = false,
+    children,
+    handleModal,
+    handleDropdown,
+    title,
+    cancelLabel = 'Cancelar',
+    confirmLabel = 'Confirmar',
+    onCancel,
+    onConfirm,
+    visible = false,
+    isLoading = false,
   }: ContainerModalProps,
 ) {
-  const modalRoot = document.getElementById('modal-root');
-
-  function handleClickOverlay(event: MouseEvent<HTMLDivElement | HTMLButtonElement>) {
+  function handleCloseModalClickOverlay(event: MouseEvent<HTMLDivElement | HTMLButtonElement>) {
     if (event.target === event.currentTarget) {
+      if (handleModal) {
+        handleModal();
+      }
+
+      if (onCancel) {
+        onCancel();
+      }
+    }
+  }
+
+  function handleCloseModal() {
+    if (handleModal) {
       handleModal();
+    }
+
+    if (onCancel) {
+      onCancel();
     }
   }
 
   function handleClickModal() {
-    handleDropdown();
+    if (handleDropdown) {
+      handleDropdown();
+    }
   }
 
-  if (!modalRoot) {
+  if (!visible && onConfirm) {
     return null;
   }
-  return reactDom.createPortal(
-    <S.Overlay onClick={(event) => handleClickOverlay(event)}>
-      <S.Container danger={danger} onClick={() => handleClickModal()}>
-        <button type="button" onClick={() => handleModal()}>
-          <S.Close>
-            <img src={CloseIcon} alt="x" />
-          </S.Close>
-        </button>
-        {children}
-      </S.Container>
-    </S.Overlay>,
-    modalRoot,
+
+  return (
+    <ReactPortal containerId="modal-root">
+      <S.Overlay onClick={(event) => handleCloseModalClickOverlay(event)}>
+        <S.Container danger={danger} onClick={() => handleClickModal()}>
+          <h1>{title}</h1>
+
+          <button type="button" onClick={handleCloseModal}>
+            <S.Close>
+              <img src={CloseIcon} alt="x" />
+            </S.Close>
+          </button>
+
+          <div className="modal-body">{children}</div>
+
+          {visible && (
+          <S.Footer>
+            <button
+              type="button"
+              className="cancel-button"
+              disabled={isLoading}
+              onClick={onCancel}
+            >
+              {cancelLabel}
+            </button>
+
+            <Button
+              type="button"
+              danger={danger}
+              isLoading={isLoading}
+              handleClick={onConfirm}
+            >
+              {confirmLabel}
+            </Button>
+          </S.Footer>
+          )}
+        </S.Container>
+      </S.Overlay>
+    </ReactPortal>
   );
 }
