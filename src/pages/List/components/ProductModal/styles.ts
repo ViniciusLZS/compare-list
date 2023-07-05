724caa95921fa29f1aa7775203433104d5ff2d14
Vinícius <vinicius.luiz.9256@gmail.com>
2023-07-05 15:09:34 -0300

    feat: container added to total

--- src/pages/List/components/ProductModal/styles.ts
+++ src/pages/List/components/ProductModal/styles.ts
@@ -1,9 +1,28 @@
 import styled from 'styled-components';
 
 export const Amount = styled.div`
+  width: 100%;
   display: flex;
   align-items: center;
   justify-content: center;
   gap: 1rem;
+`;
 
+export const TotalContainer = styled.div`
+  margin-top: 2rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  span {
+    margin-bottom: 1rem;
+    color: ${({ theme }) => theme.colors.gray[200]};
+  }
+`;
+
+export const Total = styled.div`
+  padding: 1rem;
+  border-radius: 1rem;
+  background-color: ${({ theme }) => theme.colors.default.white};
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
 `;
