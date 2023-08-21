ab6d2e1e431f9bc6d88d033d96d2ee35280df064
Vinicius <vinicius.luiz.9256@gmail.com>
2023-08-21 19:13:44 -0300

    fix: deploy

--- src/services/utils/baseURL.ts
+++ src/services/utils/baseURL.ts
@@ -1,6 +1,7 @@
 export default function baseURL() {
-  const { DATABASE_URL } = process.env;
-  const baseURLs = DATABASE_URL;
+  const DATABASE_URL = process.env.REACT_APP_NOT_SECRET_CODE;
+
+  const baseURLs = DATABASE_URL!.toString();
 
   return baseURLs;
 }
