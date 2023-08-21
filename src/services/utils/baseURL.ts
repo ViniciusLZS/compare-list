dae0e9dc3af7e0475bcaa9f365a1e9c1208fd892
Vinicius <vinicius.luiz.9256@gmail.com>
2023-08-21 16:44:53 -0300

    feat/refactor: deploy

--- src/services/utils/baseURL.ts
+++ src/services/utils/baseURL.ts
@@ -1,11 +1,6 @@
 export default function baseURL() {
-  const local = window.location.origin;
-  let baseURLs = 'http://localhost:3001';
-  if (local === 'http://localhost:3001') {
-    baseURLs = 'http://localhost:3001';
-  } else {
-    baseURLs = 'http://192.168.18.7:3001';
-  }
+  const { DATABASE_URL } = process.env;
+  const baseURLs = DATABASE_URL;
 
   return baseURLs;
 }
